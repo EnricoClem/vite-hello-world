afbb939df786860ba562ec2f0fd4a900c4bc771c
EnricoClem <enrico.clem94@gmail.com>
2024-04-09 16:54:59 +0200

    ADD - AppTitle content

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="title">
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style>
+    .title {
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+        padding: 50px;
+        color: blueviolet;
+    }
+</style>
